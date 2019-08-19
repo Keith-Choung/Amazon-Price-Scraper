b5955bb3f66da2e94d847772bc7a75a447efee2e
Keith Choung <keithchoung@Keiths-MacBook-Pro.local>
2019-08-19 10:47:36 -0700

    main fix

--- src/main_program.py
+++ src/main_program.py
@@ -1,5 +1,10 @@
 import scraper
-import organize
+
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '/Users/keithchoung/Desktop/CS/WebScraper/src')
+
+
 import cmd
 
 intro = "Welcome to your WebScraper!\nType -help to list all of the commands you can use :)"
